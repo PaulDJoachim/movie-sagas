00f98d865f1d56349b6ab381abe906615f3d73ac
Paul Joachim <PaulDJoachim@gmail.com>
2020-07-25 01:33:50 -0500

    using material UI grid list

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Movie from '../Movie/Movie'
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0,0,0,0) 100%',
+
+  },
+  gridList: {
+    width: 'auto',
+    height: 'auto',
+    justifyContent: 'space-around',
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+});
+
+
 class Home extends Component {
   componentDidMount(){
     // Get movies from database
@@ -11,20 +37,29 @@ class Home extends Component {
 
   
   render() {
+    const {classes} = this.props;
     return (
-      <div>
-        <p>Movie List</p>
-        {/* {JSON.stringify(this.props.reduxState.movies)} */}
-        {this.props.reduxState.movies.map((movie, index) => (
-          <Movie key={index} id={index}/>
-        ))}
+      <div className={classes.root}>
+        <GridList cellHeight={'auto'} className={classes.Home} cols={0}>
+          {/* {JSON.stringify(this.props.reduxState.movies)} */}
+          {this.props.reduxState.movies.map((movie, index) => (
+            <GridListTile key={index} cols={1}>
+              <Movie movie={movie}/>
+            </GridListTile>
+          ))}
+        </GridList>
       </div>
     );
   }
 }
 
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
 const mapStateToProps = reduxState => ({
   reduxState,
 });
 
-export default connect(mapStateToProps)(Home);
+export default withStyles(styles)(connect(mapStateToProps)(Home));
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -7,8 +7,8 @@ class Movie extends Component {
   render() {
     return (
       <div>
-        {this.props.reduxState.movies[this.props.id].title}
-        <img src={this.props.reduxState.movies[this.props.id].poster} />
+        {/* {this.props.movie.title} */}
+        <img src={this.props.movie.poster} />
       </div>
     );
   }
